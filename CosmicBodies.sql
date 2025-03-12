7c7784d62a2919a4d7e5e7ed2460fd5fe2f30a84
BragXyx58 <BragX_yx58@student.itstep.org>
2025-03-12 09:14:17 +0100

    Created Table in DataBase
    Created Table [CosmicBodies]  in DataBase db_comsic_bodies

--- CosmicBodies.sql
+++ CosmicBodies.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE db_comsic_bodies;
+
+USE db_comsic_bodies;
+CREATE TABLE [CosmicBodies] (
+    [ID] INT IDENTITY,
+    [Name] VARCHAR(255),
+    [Type] VARCHAR(255),
+    [DistanceToEarth] INT,
+	[Size] INT
+	[YearOfDiscovery] INT
+);
